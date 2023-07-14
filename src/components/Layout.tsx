916edd3c32c7f80f99451308a6c034111ecd3c02
Dmitry Firsanov <dmitryfirsanov03@gmail.com>
2023-07-14 21:55:38 +0300

    fix the height issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,17 +5,17 @@ import translatorAppIcon from "../assets/icons/translator-app-icon.svg";
 function Layout() {
   return (
     <>
-      <header className="header">
-        <Link className="header-logo" to="/">
-          <img src={translatorAppIcon} alt="Translator app" />
-          <p>Translator app</p>
-        </Link>
-        <div className="header-nav">
-          <Link className="nav-link" to="/text">Текст</Link>
-          <Link className="nav-link" to="/document">Документы</Link>
+      <div className="main-page">
+        <div className="header">
+          <Link className="header-logo" to="/">
+            <img src={translatorAppIcon} alt="Translator app" />
+            <p>Translator app</p>
+          </Link>
+          <div className="header-nav">
+            <Link className="nav-link" to="/text">Текст</Link>
+            <Link className="nav-link" to="/document">Документы</Link>
+          </div>
         </div>
-      </header>
-      <div className="page">
         <Outlet />
       </div>
     </>
