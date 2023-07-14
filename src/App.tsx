7dc9eb2e11c2cb51f210d3336fddd525f4758b0f
Dmitry Firsanov <dmitryfirsanov03@gmail.com>
2023-07-14 20:53:44 +0300

    add Layout for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import translatorAppIcon from "./icons/translator-app-icon.svg";
-
+import Layout from "./components/Layout";
 import TextPage from "./components/TextPage";
 import DocumentsPage from "./components/DocumentsPage";
 import ErrorPage from "./components/ErrorPage";
@@ -10,24 +9,14 @@ import ErrorPage from "./components/ErrorPage";
 function App() {
   return (
     <>
-      <header className="header">
-        <Link className="header-logo" to="/">
-          <img src={translatorAppIcon} alt="Translator app" />
-          <p className="header-logo-title">Translator app</p>
-        </Link>
-        <div className="header-nav">
-          <Link className="nav-link" to="/text">Текст</Link>
-          <Link className="nav-link" to="/document">Документы</Link>
-        </div>
-      </header>
-      <div className="page">
-        <Routes>
+      <Routes>
+        <Route path="/" element={<Layout />} >
           <Route index element={<TextPage />} />
           <Route path="text" element={<TextPage />} />
           <Route path="document" element={<DocumentsPage />} />
           <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </div>
+        </Route>
+      </Routes>
     </>
   );
 }
