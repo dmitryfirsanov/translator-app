adca664aae98bfbd7071681e276677c4d6319871
Dmitry Firsanov <dmitryfirsanov03@gmail.com>
2023-07-26 21:08:38 +0300

    configure vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig, UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-const config: UserConfig = {
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://translate.api.cloud.yandex.net/translate/v2'
-    }
-  }
-};
 
-export default defineConfig(config);
-
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+});
